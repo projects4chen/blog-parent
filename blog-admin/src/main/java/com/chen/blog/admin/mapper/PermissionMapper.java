7c16c8d55dc54f3648b9f7ea0ae102d956e02fab
cbb <470557347@qq.com>
2022-08-14 20:04:20 +0800

    build a background demo

---
+++ blog-admin/src/main/java/com/chen/blog/admin/mapper/PermissionMapper.java
@@ -0,0 +1,7 @@
+package com.chen.blog.admin.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.chen.blog.admin.pojo.Permission;
+
+public interface PermissionMapper extends BaseMapper<Permission> {
+}
